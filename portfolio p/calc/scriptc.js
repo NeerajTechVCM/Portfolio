b0b8b9d6edd284e46ec79ed46714214f1fdf3dc2
NeerajTechVCM <np6848586@gmail.com>
2024-11-09 21:39:21 +0530

    Update scriptc.js

--- portfolio p/calc/scriptc.js
+++ portfolio p/calc/scriptc.js
@@ -1,33 +1,26 @@
-let string='';
+
 let buttons=document.querySelectorAll('.button');
 let screen=document.querySelector('.screen');
 Array.from(buttons).forEach((button)=>{
 button.addEventListener('click',(e)=>{
-    console.log(e.target.innerHTML)
-    if(e.target.innerHTML=='='){
-        string=eval(string);
-        screen.value=string;
-        
 
-    }
+    if(e.target.innerHTML=='='){
+        screen.value=eval(string);
+         }
     else if(e.target.innerHTML=='AC'){
-        string='';
-     
-        screen.value=string;
+        screen.value="";
         }
     else if(e.target.innerHTML=='Del'){
-        string=string.substring(0,string.length-1);
-     
-        screen.value=string;
+        screen.value=string.substring(0,string.length-1);
     }
     else if(e.target.innerHTML=='%'){
         
-        screen.value=string/100;
+        screen.value=screen.value/100;
     }
    
     else{
-        string=string+e.target.innerHTML;
-        screen.value=string;
+        screen.value=screen.value+e.target.innerHTML;
+    
     }
 })
 })
